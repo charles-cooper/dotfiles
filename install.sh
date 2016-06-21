3ba2a2cd7275b1b0c64fab8ec72ba2c25a38103e
Charles Cooper <cooper.charles.m@gmail.com>
2016-06-21 15:29:08 -0400

    Remove vundle from installation

--- install.sh
+++ install.sh
@@ -2,6 +2,3 @@
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/gitconfig ~/.gitconfig
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cd ~/.vim/bundle/Vundle.vim && git pull
-
