6dea3be2de381c49ce048b7ecd0658ae18c9c929
Charles Cooper <cooper.charles.m@gmail.com>
2017-06-04 12:01:34 -0700

    Install solidity vim files

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/gitconfig ~/.gitconfig
 
+git submodule update --init
+rsync -av --include="*/" --include="solidity.vim" --exclude="*" vim-solidity/ ~/.vim/
+
