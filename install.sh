cd35de1c808cc83214daccd2a08b64f5562b4ada
Charles Cooper <cooper.charles.m@gmail.com>
2017-10-19 23:42:08 -0700

    Add vim-pug

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ ln -s $(pwd)/gitconfig ~/.gitconfig
 git submodule update --init
 rsync -av --include="*/" --include="solidity.vim" --exclude="*" vim-solidity/ ~/.vim/
 rsync -av --include="*/" --include="rust.vim" --exclude="*" rust.vim/ ~/.vim/
-rsync -av --include="*/" --include="idris-vim" --exclude="*" idris-vim/ ~/.vim/
+rsync -av --include="*/" --include="idris.vim" --exclude="*" idris-vim/ ~/.vim/
+rsync -av --include="*/" --include="pug.vim" --exclude="*" vim-pug/ ~/.vim/
 
