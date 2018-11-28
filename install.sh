153f6aaf67f2a6e86a785041bfd4b5c449c52bbc
Charles Cooper <cooper.charles.m@gmail.com>
2018-11-28 10:43:42 -0800

    Add vyper, elm vim submodules

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ rsync -av --include="*/" --include="solidity.vim" --exclude="*" vim-solidity/ ~/
 rsync -av --include="*/" --include="rust.vim" --exclude="*" rust.vim/ ~/.vim/
 rsync -av --include="*/" --include="idris.vim" --exclude="*" idris-vim/ ~/.vim/
 rsync -av --include="*/" --include="pug.vim" --exclude="*" vim-pug/ ~/.vim/
-
+rsync -av --include="*/" --include="elm.vim" --exclude="*" elm-vim/ ~/.vim/
+rsync -av --include="*/" --include="vyper.vim" --exclude="*" vim-vyper/ ~/.vim/
