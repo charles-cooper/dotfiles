e7e35c0b1aa180e7bab6dde2390b553c9282b1ac
Charles Cooper <cooper.charles.m@gmail.com>
2019-06-26 19:42:14 -0700

    Add vlang syntax

--- install.sh
+++ install.sh
@@ -10,3 +10,7 @@ rsync -av --include="*/" --include="pug.vim" --exclude="*" vim-pug/ ~/.vim/
 rsync -av --include="*/" --include="elm.vim" --exclude="*" elm-vim/ ~/.vim/
 rsync -av --include="*/" --include="vyper.vim" --exclude="*" vim-vyper/ ~/.vim/
 rsync -av --include="*/" --include="vue.vim" --exclude="*" vim-vue/ ~/.vim/
+
+rsync -av --include="*/" --include="vlang.vim" --exclude="*" v-vim/ ~/.vim/
+# v-vim doesn't use standard ftdetect file, instead it messes with filetype.vim
+rsync -av --include="*/" --include="filetype.vim" --exclude="*" v-vim/ ~/.vim/
